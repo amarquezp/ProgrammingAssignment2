7f9871a9191dbcb0252f916dc5512db63d574b5c
QCT B2B <qctb2b@qualcomm.com>
2017-07-23 12:52:50 -0500

    Completing programming assignment for the week 3 of Coursera's Data Specialization course

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Antonio Marquez Palacios
+## Cousera Data Scientist Specialization
+## Programming Assignment 2: Lexical Scoping
+# July 23, 2017
 
-## Write a short comment describing this function
+## As per Programming Assignment 2: Lexical Scoping of the Data Scientist Specialization
+## This function is intended to create a special "matrix" object that can cache its inverse
+
+## function :  makeCacheMatrix
+## arguments: 'x' as a matrix object
+## returns  : a special object 'matrix' which is a list of functions for caching the inverse value
+## usage    : B <- makeCacheMatrix(matrix(c(1,2,3,4,5,6,7,8,9), nrow=3, ncol=3))
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  m <- NULL
+  
+  set <- function(y){
+    
+    x <<- y
+    m <<- NULL
+    
+  }
+  
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## function : cacheSolve
+## agruments: x: a matrix created with makeCacheMatrix() function
+## returns  : the inverse of the given matrix
+## usage    : cacheSolve(B)
+## NOTE     : it is assumed 'x' to be an invertible matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  m <- x$getinverse()
+  
+  if( !is.null(m) ){
+    
+    message("inverse is cached, returning from cache")
+    return(m)
+    
+  }
+  
+  data <- x$get()
+  m <- solve(data)
+  x$setinverse(m)
+  m
+  
+        
 }
